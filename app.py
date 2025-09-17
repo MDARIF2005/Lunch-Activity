25ec5dcc19e31e076322fb11e5caad8831ee9ef9
MDARIF2005 <arifsmart2005@gmail.com>
2025-09-17 10:44:49 +0530

    comited for host

--- app.py
+++ app.py
@@ -678,7 +678,7 @@ def cleanup():
 
 if __name__ == '__main__':
     try:
-        app.run(debug=True, port=5000)
+        app.run(debug=True, host='0.0.0.0', port=5000)
     finally:
         # Clean up camera on exit
         with camera_lock:
